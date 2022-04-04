cbb87fa06265ec87f8c252b1ee3ec78511ff06c1
JoelCAI <joelargentina12345@gmail.com>
2022-04-03 22:48:18 -0300

    Agregar archivos de proyecto.

---
+++ LaCadena1/Sistema.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LaCadena1
+{
+    public class Sistema
+    {
+        public void MenuPrincipal()
+        {
+            string nombre;
+
+            nombre = Validador.ValidarString("\n Ingrese su nombre");
+            Validador.ConcatenarTexto(nombre);
+ 
+            Validador.Despedida();
+        }
+
+        public void Iniciar()
+        {
+            MenuPrincipal();
+        }
+    }
+}
---
+++ LaCadena1/Validador.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LaCadena1
+{ 
+    public class Validador
+    {
+
+        public static void ConcatenarTexto(string nombre)
+        {
+            Console.Clear();
+            Console.WriteLine("\n Hola " + "*" + nombre + "*");
+            
+        }
+
+        public static void Despedida()
+        {
+
+            Console.WriteLine("\n Gracias por usar nuestro Sistema presione cualquier teclar para finalizar");
+            Console.ReadKey();
+        }
+
+        public static string ValidarString(string mensaje)
+        {
+
+            string opcion;
+            do
+            {
+
+                Console.WriteLine(mensaje);
+                opcion = Console.ReadLine();
+                if (string.IsNullOrEmpty(opcion))
+                {
+                    Console.Clear();
+                    Console.WriteLine(" \nEl valor ingresado no puede ser vacio.");
+                    Console.WriteLine(" Por favor ingrese un valor.");
+                }
+            } while (string.IsNullOrEmpty(opcion));
+            return opcion;
+
+        }
+    }
+}
